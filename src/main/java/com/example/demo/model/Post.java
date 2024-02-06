2867714b06a195db059659553930e80c84c20286
Yana Gnedkova <ygnedkova@gmail.com>
2024-02-06 17:54:28 +0500

    Bootstrap вёрстка

---
+++ src/main/java/com/example/demo/model/Post.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class Post {
+
+    private String text;
+    private Integer likes;
+    private Date creationDate;
+
+    public Post(String text, Date creationDate){
+        this.text = text;
+        this.creationDate = creationDate;
+        this.likes = 0;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public Integer getLikes(){
+        return likes;
+    }
+
+    public Date getCreationDate(){
+        return creationDate;
+    }
+}
