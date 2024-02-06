3d7efb6dcd811b8b6224c96925a1b576c7f6a788
Yana Gnedkova <ygnedkova@gmail.com>
2024-02-06 18:45:36 +0500

    Создание нового поста

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,23 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
-    public List<Post> listAllPosts()
-    {
-        return Arrays.asList(new Post("post 1", new Date()), new Post("post 2", new Date()), new Post("post 3", new Date()));
-    }
 
+    private ArrayList<Post> posts = new ArrayList<>(Arrays.asList(
+            new Post("post 1", new Date()),
+            new Post("post 2", new Date()),
+            new Post("post 3", new Date())));
+
+    public List<Post> listAllPosts(){
+        return posts;
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
+    }
 }
