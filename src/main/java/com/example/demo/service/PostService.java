7d8a234d8bc6c17290ff4a7076763de339bf24b0
Yana Gnedkova <ygnedkova@gmail.com>
2024-02-06 19:17:23 +0500

    patch

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,25 +1,28 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
 
-    private ArrayList<Post> posts = new ArrayList<>(Arrays.asList(
-            new Post(0L,"post 1", new Date()),
-            new Post(1L, "post 2", new Date()),
-            new Post(2L,"post 3", new Date())));
+    @Autowired
+    PostRepository postRepository;
 
-    public List<Post> listAllPosts(){
-        return posts;
+    public List<Post> listAllPosts() {
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
-    public void create(String text) {
-        posts.add(new Post(null, text, new Date()));
+
+    public void create(final String text) {
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
